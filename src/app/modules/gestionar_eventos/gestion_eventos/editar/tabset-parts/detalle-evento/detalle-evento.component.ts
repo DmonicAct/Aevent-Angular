f135a51e0b990d751a118b0931f949f0377318a7
agarcias12 <50123104+agarcias12@users.noreply.github.com>
2019-09-30 18:49:42 -0500

    Cambios en la ventana de creación de eventos

--- src/app/modules/gestionar_eventos/gestion_eventos/editar/tabset-parts/detalle-evento/detalle-evento.component.ts
+++ src/app/modules/gestionar_eventos/gestion_eventos/editar/tabset-parts/detalle-evento/detalle-evento.component.ts
@@ -15,14 +15,43 @@ export class DetalleEventoConfiguracion implements OnInit {
         this.item = new Evento();
     }
     @ViewChild('autoShownModal') autoShownModal: ModalDirective;
-    isModalShown = false;
+    isModalShownPresidente = false;
+    isModalShownCategorias = false;
     ngOnInit(): void {
 
     }
     datos: boolean = true;
     call: boolean = false;
     fases: boolean = false;
+    modalPresidenteCorrecto: boolean = false;
 
+
+    categoriasMaestro = [
+        {id: 1, nombre: 'Gobierno Electrónico', check: false},
+        {id: 2, nombre: 'Marketing Digital', check: false},
+        {id: 3, nombre: 'Bussiness Inteligence', check: false},
+        {id: 4, nombre: 'Inteligencia Artificial', check: false},
+        {id: 5, nombre: 'Lean IT', check: false},
+        {id: 6, nombre: 'Gráficos de Computadoras', check: false},
+    ];
+
+    maestroPresidentes = [
+        {id: 1, nombre: 'Luis Flores', check: false},
+        {id: 2, nombre: 'César Aguilera', check: false},
+    ];
+
+    evento = {
+        id: 22,
+    }
+    presidente = {
+        id: 0,
+        nombre: 'Agregar Presidente',
+        check: false,
+    }
+    categorias = [
+        {id: 0, nombre: 'Agregar Categorías'},
+    ];
+    
     verDatos(event) {
         this.datos = true;
         this.call = false;
@@ -41,18 +70,65 @@ export class DetalleEventoConfiguracion implements OnInit {
         this.fases = true;
     }
     DetectChange(){}
-    OnAgregarCategorias() { }
-    OnAgregarPresidente() {
-        this.isModalShown = true;
+    OnAgregarCategorias() { 
+        this.isModalShownCategorias = true;
     }
-    hideModal(): void {
+    hideModalCategorias(): void {
         this.autoShownModal.hide();
     }
 
-    onHidden(): void {
-        this.isModalShown = false;
+    onHiddenCategorias(): void {
+        this.isModalShownCategorias = false;
     }
-    onNuevo() {
-        this.isModalShown = false;
+    onAceptarCategorias() {
+        let aux = [];
+        this.categoriasMaestro.forEach(function (value){
+            if (value.check == true){
+                aux.push(value);
+            }
+        });
+        this.categorias = aux;
+        this.isModalShownCategorias = false;
+    }
+
+
+    OnAgregarPresidente() {
+        this.isModalShownPresidente = true;
+    }
+    hideModalPresidente(): void {
+        this.autoShownModal.hide();
+    }
+
+    onHiddenPresidente(): void {
+        this.isModalShownPresidente = false;
+    }
+    onAceptarPresidente() {
+        let aux = {
+            id: 0,
+            nombre: 'Agregar Presidente',
+            check: false,
+        };
+        let count = 0;
+        this.isModalShownPresidente = false;
+        this.maestroPresidentes.forEach(function (value){
+            if (value.check == true){
+                count += 1;
+                aux = value;
+            }
+        });
+        if (count > 1){
+            console.log("Esta mal");
+            this.modalPresidenteCorrecto = true;
+            this.presidente = {
+                id: 0,
+                nombre: 'Agregar Presidente',
+                check: false,
+            }
+        } else if (count == 1){
+            console.log("Esta bien");
+            this.presidente = aux;
+            this.isModalShownCategorias = false;
+            this.modalPresidenteCorrecto = false;
+        }
     }
 }
