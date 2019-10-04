7443badc08b70feff769cc44a3aa2a749e804573
DmonicAct <lck_23@hotmail.com>
2019-10-04 16:49:12 -0500

    Cambios en call for papers

--- src/app/models/pregunta.ts
+++ src/app/models/pregunta.ts
@@ -1,7 +1,10 @@
 import {TipoPregunta} from './enums/tipo-pregunta';
 export class Pregunta{
     public idPregunta: number;
+    public indice: number;
+    public cabecera: Boolean;
     public descripcion: string;
-    public subDescipcion: string; //cambiar en spring
+    //public subDescipcion: string; //cambiar en spring
     public tipoPregunta: TipoPregunta;
+    public cantidadLetras: number;
 }
