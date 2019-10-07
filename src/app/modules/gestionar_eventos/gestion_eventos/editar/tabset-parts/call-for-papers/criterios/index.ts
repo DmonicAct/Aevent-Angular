f471aeae1cab0a475b516451ff48fcb53b9fcc2c
DmonicAct <lck_23@hotmail.com>
2019-10-07 01:28:20 -0500

    commit call for paper: vista de documento dinamico - solo preguntas abiertas

--- src/app/modules/gestionar_eventos/gestion_eventos/editar/tabset-parts/call-for-papers/criterios/index.ts
+++
@@ -1,9 +0,0 @@
-import {CriterioOpcionMultiple}  from './criterio-opcion-multiple.component';
-import {CriterioPreguntaAbierta}  from './criterio-pregunta-abierta.component';
-import {CriterioFormularioComponent}  from './criterio-pregunta-formulario.component';
-
-export{
-    CriterioOpcionMultiple,
-    CriterioPreguntaAbierta,
-    CriterioFormularioComponent
-}
