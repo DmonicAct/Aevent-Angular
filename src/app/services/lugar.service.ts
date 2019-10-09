30a5d3824dd619472e8b247cd024c832a209a4ea
hbustamanter <a20145273@pucp.pe>
2019-10-09 17:49:32 -0500

    lugarservice

---
+++ src/app/services/lugar.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import { Observable,throwError } from 'rxjs';
+import {  catchError } from 'rxjs/operators';
+  import {Categoria} from '../models/categoria';
+import {Lugar} from '../models/lugar';
+@Injectable({
+    providedIn: 'root',
+  })
+
+export class LugarService{
+    private apiEndpoint: string;
+
+    private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+  
+    constructor(public http: HttpClient) {
+      this.apiEndpoint = environment.serviceEndpoint + '/lugar';
+  
+    }
+
+    obtenerLugares():Observable<any> {
+        return this.http.get(this.apiEndpoint).pipe(
+        catchError(e => {
+            if (e.status == 400) {
+            return throwError(e);
+            }
+            if (e.error.mensaje) {
+            console.error(e.error.mensaje);
+            }
+            return throwError(e);
+        }));
+    }
+}
