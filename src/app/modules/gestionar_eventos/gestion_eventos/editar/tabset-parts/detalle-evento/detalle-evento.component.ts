e119c0a7dabb85315d456a9c703425530ec09f4a
hbustamanter <a20145273@pucp.pe>
2019-11-08 20:58:17 -0500

    Visualizar Presidente en detalle evento al seleccionarlo.

--- src/app/modules/gestionar_eventos/gestion_eventos/editar/tabset-parts/detalle-evento/detalle-evento.component.ts
+++ src/app/modules/gestionar_eventos/gestion_eventos/editar/tabset-parts/detalle-evento/detalle-evento.component.ts
@@ -274,7 +274,8 @@ export class DetalleEventoConfiguracion implements OnInit {
 
     ElegirPresidente(data){
         this.item.presidente = data;
-        this.nombrePresidente = this.item.presidente.fullName;
+        
+        this.nombrePresidente = this.item.presidente.nombreCompleto;
     }
 
     OnAceptarPresidente() {
