511de1c3b4cae1e54c99137d46a2302f50d922fb
DmonicAct <lck_23@hotmail.com>
2019-11-10 17:51:56 -0500

    Creacion de propuesta

--- src/app/models/respuesta_postulacion.ts
+++ src/app/models/respuesta_postulacion.ts
@@ -1,7 +1,7 @@
 import { Postulacion } from "./postulacion";
 import { RespuestaFormulario } from ".";
 
-export class respuestaPostulacion{
+export class RespuestaPostulacion{
     public postulacion:Postulacion;
     public listaFormulario:Array<RespuestaFormulario>;
 }
---
+++ src/app/modules/convocatoria/postular/detalle-propuesta/detalle-propuesta.component.ts
@@ -0,0 +1,12 @@
+import { OnInit, Component } from "@angular/core";
+
+@Component({
+    selector:'detalle-propuesta',
+    templateUrl:'detalle-propuesta.template.html',
+    styleUrls:['detalle-propuesta.template.scss']
+})
+export class DetallePropuestaComponent implements OnInit{
+    ngOnInit(){
+
+    }
+}
