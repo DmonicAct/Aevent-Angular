586bf4d50a6f1f6f73121310a194ceef414f9928
DmonicAct <lck_23@hotmail.com>
2019-11-13 19:29:31 -0500

    Commit: Error de Topnavar solucionado

--- src/app/components/common/topnavbar/topnavbar.component.ts
+++ src/app/components/common/topnavbar/topnavbar.component.ts
@@ -3,9 +3,9 @@ import { smoothlyMenu } from '../../../app.helpers';
 import { AppSettings } from '../../../app.settings';
 import { Router } from '@angular/router';
 import { Estado, Response } from '../../../models';
-import { AuthService } from 'src/app/auth/service/auth.service';
 import { Persona, Usuario } from 'src/app/models';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { AuthService as AeventAuthService } from '../../../auth/service/auth.service';
 declare var jQuery:any;
 
 @Component({
@@ -15,26 +15,28 @@ declare var jQuery:any;
 export class TopNavbarComponent  implements OnInit{
 
   public usr: Usuario;
-  ngOnInit(): void {
+  constructor(private router: Router, private auth: AeventAuthService,    
+    private usrService: UsuarioService) {
+    this.nombre = AppSettings.APP_NAME;
+    this.usuario = auth.usuario;
+    this.persona = auth.persona;
+    }
+  ngOnInit() {
     this.usrService.obtenerUsuarioUs(this.auth.usuario.username).subscribe(
       (response:Response)=>{
           this.usr = response.resultado;
           console.log(this.usr)
         
       }
-  )
+    );
   }
+
 
   nombre:string;
   usuario: Usuario;
   persona: Persona;
 
-  constructor(private router: Router, private auth: AuthService,    
-        private usrService: UsuarioService) {
-    this.nombre = AppSettings.APP_NAME;
-    this.usuario = auth.usuario;
-    this.persona = auth.persona;
-  }
+
 mostrarNotif(){
   this.usr.evaluacionSinLeer=false;
   this.usrService.guardarUsuarioSistema(<Persona>this.usr).subscribe(
@@ -55,7 +57,7 @@ mostrarNotif2(){
   }
   OnLogout(){
     this.auth.logout();
-    this.router.navigate(["/login"]);
+    this.router.navigate(['/login']);
     //routerLink="/login"
   }
 }
