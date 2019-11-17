17acf981353e5287c0a678e1fd6ae2499c49e6ef
IzayaKito <juanpa1920@gmail.com>
2019-11-16 20:31:20 -0500

    [MERGE] Fixes en Seleccion de presidente de evento

--- src/app/modules/mantenimiento/mantenimiento.module.ts
+++ src/app/modules/mantenimiento/mantenimiento.module.ts
@@ -13,10 +13,8 @@ import {GestionUsuarioListaComponent} from './gestionUsuarios/lista.component';
 import {EditarUsuarioComponent}from './gestionUsuarios/editar/editar.component';
 
 import {GestionCategoriaListaComponent} from './GestionCategorias/lista.component';
-import {EditarCategoriaComponent}from './GestionCategorias/editar/editar.component';
 
 import {GestionTipoEventoListaComponent} from './GestionTipoEvento/lista.component';
-import {EditarTipoEventoComponent}from './GestionTipoEvento/editar/editar.component';
 
 import {GestionLugarListaComponent} from './GestionLugar/lista.component';
 
@@ -31,9 +29,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     GestionUsuarioListaComponent,
     EditarUsuarioComponent,
     GestionCategoriaListaComponent,
-    EditarCategoriaComponent,
     GestionTipoEventoListaComponent,
-    EditarTipoEventoComponent,
     GestionLugarListaComponent,
     GestionTipoCriterioListaComponent,
   ],
