d694e12e8ff54d6e2e035898127411e801954c6a
DmonicAct <lck_23@hotmail.com>
2019-11-22 17:35:48 -0500

    Nuevo ambiente de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 //const url: string='192.168.1.13:8090/aevent/';
 //const url: string='http://localhost:8090/aevent/';
-const url: string='http://ec2-54-92-163-89.compute-1.amazonaws.com:8080/aevent/'
+//const url: string='http://ec2-54-92-163-89.compute-1.amazonaws.com:8080/aevent/'
+const url: string='http://ec2-18-206-239-108.compute-1.amazonaws.com:8080/aevent/'
+//http://ec2-18-206-239-108.compute-1.amazonaws.com:8080/
 export const environment = {
   production: false,
   serviceBackEndpoint: url,
